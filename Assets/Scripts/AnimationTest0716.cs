77adf080da2c2d612462d4a872b8b4956ff8241b
Hoz94 <sotech3911@gmail.com>
2024-07-17 10:25:01 +0900

    f

---
+++ Assets/Scripts/AnimationTest0716.cs
@@ -0,0 +1,293 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest0716 : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
+    void Update()
+    {
+        #region 검 애니
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SwordAnimation1();
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            SwordAnimation2();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            SwordAnimation3();
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            SwordAnimation4();
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            SwordAnimation5();
+        }
+        else if (Input.GetKeyDown(KeyCode.Y))
+        {
+            SwordAnimation6();
+        }
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
+            SwordAnimation7();
+        }
+
+
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            SwordShieldAnimation1();
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            SwordShieldAnimation2();
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            SwordShieldAnimation3();
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            SwordShieldAnimation4();
+        }
+        #endregion
+
+        #region 도끼 애니
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            AxeAnimation1();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            AxeAnimation2();
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            AxeAnimation3();
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            AxeAnimation4();
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            AxeAnimation5();
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            AxeAnimation6();
+        }
+
+
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            AxeAnimation7();
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            AxeAnimation8();
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            AxeAnimation9();
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            AxeAnimation10();
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            AxeAnimation11();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            AxeAnimation12();
+        }
+        #endregion
+
+        #region 메이지 애니
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            MageAnimation1();
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            MageAnimation2();
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            MageAnimation3();
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            MageAnimation4();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            MageAnimation5();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MageAnimation6();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            MageAnimation7();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            MageAnimation8();
+        }
+       
+        #endregion
+    }
+
+    #region 검 애니
+    private void SwordAnimation1()
+    {
+        animator.CrossFade("2Hand-Sword-Attack1", .2f);
+    }
+    private void SwordAnimation2()
+    {
+        animator.CrossFade("2Hand-Sword-Attack2", .2f);
+    }
+    private void SwordAnimation3()
+    {
+        animator.CrossFade("2Hand-Sword-Attack3", .2f);
+    }
+    private void SwordAnimation4()
+    {
+        animator.CrossFade("2Hand-Sword-Attack4", .2f);
+    }
+    private void SwordAnimation5()
+    {
+        animator.CrossFade("2Hand-Sword-Attack5", .2f);
+    }
+    private void SwordAnimation6()
+    {
+        animator.CrossFade("2Hand-Sword-Attack6", .2f);
+    }
+    private void SwordAnimation7()
+    {
+        animator.CrossFade("Two Hand Sword Combo", .2f);
+    }
+
+
+    private void SwordShieldAnimation1()
+    {
+        animator.CrossFade("Sword And Shield Attack", .2f);
+    }
+    private void SwordShieldAnimation2()
+    {
+        animator.CrossFade("Sword And Shield Slash1", .2f);
+    }
+    private void SwordShieldAnimation3()
+    {
+        animator.CrossFade("Sword And Shield Slash2", .2f);
+    }
+    private void SwordShieldAnimation4()
+    {
+        animator.CrossFade("Sword And Shield Slash3", .2f);
+    }
+    #endregion
+
+    #region 도끼 애니
+    private void AxeAnimation1()
+    {
+        animator.CrossFade("Standing Melee Attack 360 High", .2f);
+    }
+    private void AxeAnimation2()
+    {
+        animator.CrossFade("Standing Melee Attack Backhand", .2f);
+    }
+    private void AxeAnimation3()
+    {
+        animator.CrossFade("Standing Melee Combo Attack Ver1", .2f);
+    }
+    private void AxeAnimation4()
+    {
+        animator.CrossFade("Standing Melee Combo Attack Ver2", .2f);
+    }
+    private void AxeAnimation5()
+    {
+        animator.CrossFade("Standing Melee Run Jump Attack", .2f);
+    }
+
+
+    private void AxeAnimation6()
+    {
+        animator.CrossFade("axeSwing1", .2f);
+    }
+    private void AxeAnimation7()
+    {
+        animator.CrossFade("axeSwing2", .2f);
+    }
+    private void AxeAnimation8()
+    {
+        animator.CrossFade("axeSwing3", .2f);
+    }
+    private void AxeAnimation9()
+    {
+        animator.CrossFade("axeSwing4", .2f);
+    }
+    private void AxeAnimation10()
+    {
+        animator.CrossFade("axeSwing5", .2f);
+    }
+    private void AxeAnimation11()
+    {
+        animator.CrossFade("axeSwing6", .2f);
+    }
+    private void AxeAnimation12()
+    {
+        animator.CrossFade("axeSwing7", .2f);
+    }
+
+    #endregion
+
+    #region 메이지 애니
+    private void MageAnimation1()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Attack02_Demo", .2f);
+    }
+    private void MageAnimation2()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Attack03_Demo", .2f);
+    }
+    private void MageAnimation3()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Attack06_Demo", .2f);
+    }
+    private void MageAnimation4()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Combo03_All_Demo", .2f);
+    }
+    private void MageAnimation5()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Skill01_Demo", .2f);
+    }
+    private void MageAnimation6()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Skill02_Demo", .2f);
+    }
+    private void MageAnimation7()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Skill03_Demo", .2f);
+    }
+    private void MageAnimation8()
+    {
+        animator.CrossFade("Frank_RPG_Mage_Skill04_Demo", .2f);
+    }
+    #endregion
+
+}
