b8f3605e3c5543d820f659f8f8e85cb07b4c6cbd
damsebi <donoon0518@gmail.com>
2024-07-19 04:45:49 +0900

    1차 수정

--- Assets/Scripts/UIDropDownHandler.cs
+++ Assets/Scripts/UIDropDownHandler.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,70 +10,54 @@ public class UIDropdownHandler : MonoBehaviour
     public Dropdown[] dropdowns;
 
     [SerializeField]
-    List<AnimationMapping> mappingList;
-    private Dictionary<int, string> mappingDictionary;
+    private List<AnimationMapping> mappingList;
+    private Dictionary<string, string> mappingDictionary;
     #endregion
 
     #region Start()
     void Start()
     {
-        initializeDropdownOptions();
+        InitializeDropdownOptions();
         for (int i = 0; i < dropdowns.Length; i++)
         {
-            int index = i;
-            dropdowns[i].onValueChanged.AddListener(value => { DropdownValueChange(index, value); });
+            int index = i;  // local variable to prevent closure issue
+            dropdowns[i].onValueChanged.AddListener(value => DropdownValueChange(index, value));
         }
     }
     #endregion
 
     #region 드롭다운, 맵핑 초기화
-    private void initializeDropdownOptions()
+    private void InitializeDropdownOptions()
     {
-        mappingDictionary = new Dictionary<int, string>();
+        mappingDictionary = new Dictionary<string, string>();
 
         foreach (var mapping in mappingList)
         {
-            int index = mappingList.IndexOf(mapping);
-            mappingDictionary.Add(index, mapping.animationName);
-
-            if (index < dropdowns.Length)
-            {
-                Dropdown dropdown = dropdowns[index];
-                dropdown.ClearOptions();
-                List<string> options = new List<string> { mapping.dropdownOption };
-                dropdown.AddOptions(options);
-            }
+            mappingDictionary[mapping.dropdownOption] = mapping.animationName;
         }
 
-        #region 
-        //List<string> options = new List<string>();
-
-        //dropdown.ClearOptions();
-
-        //for(int i = 0; i < mappingList.Count; i++)
-        //{
-        //    options.Add(mappingList[i].dropdownOption);
-        //    mappingDictionary.Add(i, mappingList[i].animationName);
-        //}
-        //dropdown.AddOptions(options);
-        #endregion
+        for (int i = 0; i < dropdowns.Length; i++)
+        {
+            List<string> options = new List<string>();
+            foreach (var mapping in mappingList)
+            {
+                options.Add(mapping.dropdownOption);
+            }
+            dropdowns[i].ClearOptions();
+            dropdowns[i].AddOptions(options);
+        }
     }
     #endregion
 
     #region 인덱스 확인
     void DropdownValueChange(int index, int value)
     {
-        if (mappingDictionary.TryGetValue(index, out string animationName) && !string.IsNullOrEmpty(animationName))
+        string selectedOption = dropdowns[index].options[value].text;
+
+        if (mappingDictionary.TryGetValue(selectedOption, out string animationName))
         {
             PlayerConJS.SetAnimation(index, animationName);
         }
     }
-    #endregion
-
-    #region
-    //private void PlayAnimation(string animationName)
-    //{
-    //    animator.Play(animationName);
-    //}
     #endregion
 }
